de0b051e58e6d8f2da3bf636261cb1b94c110d6c
Hodjy <62711261+Hodjy@users.noreply.github.com>
2020-10-08 18:53:16 +0300

    Cleaning the code
    Made a new file (ObjectDetectionMethods) and orginized all the program methods there.

---
+++ ObjectDetectionMethods.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+
+def __createRectanglesFromLocations(rectangles, locations, w, h):
+    for loc in locations:
+        rect = [int(loc[0]), int(loc[1]), w, h]
+        rectangles.append(rect)  # append two times in cause there is only one rectangle around an area so
+        rectangles.append(rect)  # groupRectangles wont remove it.
+
+
+def DrawRectanglesOnImage(rectangles, imgToDrawOn):
+    lineColor = (255, 0, 0)
+    lineType = cv2.LINE_4
+
+    for (x, y, w, h) in rectangles:
+        topLeft = (x, y)
+        bottomRight = (x + w, y + h)
+        cv2.rectangle(imgToDrawOn, topLeft, bottomRight, lineColor, lineType)
+
+
+def __detectFalsePositiveByNegetiveTemplate(imgToDetect, rec, template, threshold):
+    x = rec[0]
+    y = rec[1]
+    w = rec[2]
+    h = rec[3]
+    roi = imgToDetect[y:y + h, x:x + w]
+    isFalsePositive = False
+    isTemplateSmaller = roi.shape[0] > template.shape[0] and roi.shape[1] > template.shape[1]
+
+    if isTemplateSmaller:  # check for false positive.
+        matchResult = cv2.matchTemplate(roi, template, cv2.TM_CCOEFF_NORMED)
+
+        locations = np.where(matchResult >= threshold)  # get all the suspect points from the result by the threshold.
+        locations = list(zip(*locations[::-1]))
+
+        if len(locations):
+            print("Locations:", len(locations))
+            isFalsePositive = True
+
+    return isFalsePositive
+
+
+def __detectObjectByTemplate(imgToDetect, template, threshold, rectangles):  # inserts result values into rectangles.
+    templateWidth = template.shape[1]
+    templateHeight = template.shape[0]
+    # template = cv2.resize(template, None, None, 0.9, 0.9, cv2.INTER_CUBIC)
+    cv2.Laplacian(template, cv2.CV_16U, template, 3)  # sharpen template for more accuracy
+    matchResult = cv2.matchTemplate(imgToDetect, template, cv2.TM_CCOEFF_NORMED)
+    locations = np.where(matchResult >= threshold)  # get all the suspect points from the result by the threshold.
+    locations = list(zip(*locations[::-1]))  # change the presentation type of the points into tuples.
+    __createRectanglesFromLocations(rectangles, locations, templateWidth, templateHeight)
+
+def DetectPositive(imgToDetect, PTemplateList, positiveThreshold, rectangles):
+    for template in PTemplateList:
+        __detectObjectByTemplate(imgToDetect, template, positiveThreshold, rectangles)
+
+
+def DetectNegative(imgToDetect, NTemplateList, rectangles, negetiveThreshold):
+    for template in NTemplateList:
+        tempRectangles = []
+        #   template = cv2.resize(template, None, None, 0.1, 0.1, cv2.INTER_CUBIC)
+        #   cv2.Laplacian(template, cv2.CV_16U, template, 3)  # sharpen template for more accuracy
+        for rec in rectangles:
+            isFalsePositive = __detectFalsePositiveByNegetiveTemplate(imgToDetect, rec, template,
+                                                                    negetiveThreshold)
+            if not isFalsePositive:
+                tempRectangles.append(rec)
+
+        rectangles = tempRectangles
+        print(len(rectangles))
--- main.py
+++ main.py
@@ -1,93 +1,28 @@
 import glob
 import cv2
-import numpy as np
+import ObjectDetectionMethods as ODM
 
 
-def CreateRectanglesFromLocations(rectangles, locations, w, h):
-    for loc in locations:
-        rect = [int(loc[0]), int(loc[1]), w, h]
-        rectangles.append(rect)  # append two times in cause there is only one rectangle around an area so
-        rectangles.append(rect)  # groupRectangles wont remove it.
-
-
-def DrawRectanglesOnImage(rectangles, imgToDrawOn):
-    lineColor = (255, 0, 0)
-    lineType = cv2.LINE_4
-
-    for (x, y, w, h) in rectangles:
-        topLeft = (x, y)
-        bottomRight = (x + w, y + h)
-        cv2.rectangle(imgToDrawOn, topLeft, bottomRight, lineColor, lineType)
-
-
-def DetectFalsePositiveByNegetiveTemplate(imgToDetect, rec, template, threshold):
-    x = rec[0]
-    y = rec[1]
-    w = rec[2]
-    h = rec[3]
-    roi = imgToDetect[y:y + h, x:x + w]
-    isFalsePositive = False
-    isTemplateSmaller = roi.shape[0] > template.shape[0] and roi.shape[1] > template.shape[1]
-
-    if isTemplateSmaller:  # check for false positive.
-        matchResult = cv2.matchTemplate(roi, template, cv2.TM_CCOEFF_NORMED)
-
-        locations = np.where(matchResult >= threshold)  # get all the suspect points from the result by the threshold.
-        locations = list(zip(*locations[::-1]))
-
-        if len(locations):
-            print("Locations:", len(locations))
-            isFalsePositive = True
-
-    return isFalsePositive
-
-
-def DetectObjectByTemplate(imgToDetect, template, threshold, rectangles):  # inserts result values into rectangles.
-    templateWidth = template.shape[1]
-    templateHeight = template.shape[0]
-    # template = cv2.resize(template, None, None, 0.9, 0.9, cv2.INTER_CUBIC)
-    cv2.Laplacian(template, cv2.CV_16U, template, 3)  # sharpen template for more accuracy
-    matchResult = cv2.matchTemplate(imgToDetect, template, cv2.TM_CCOEFF_NORMED)
-    locations = np.where(matchResult >= threshold)  # get all the suspect points from the result by the threshold.
-    locations = list(zip(*locations[::-1]))  # change the presentation type of the points into tuples.
-    CreateRectanglesFromLocations(rectangles, locations, templateWidth, templateHeight)
-
-
-PositiveTemplateNames = glob.glob("ReferenceImages/P/*.jpg")
+PositiveTemplateNames = glob.glob("ReferenceImages/P/4.png")
 NegetiveTemplateNames = glob.glob("ReferenceImages/N/*.png")
 PTemplateList = [cv2.imread(img, 0) for img in PositiveTemplateNames]
 NTemplateList = [cv2.imread(img, 0) for img in NegetiveTemplateNames]
 imgToDetect = cv2.imread("ReferenceImages/ImagesToDetect/4.jpg", 0)
 rectangles = []
-rectanglesEps = 0.5
-positiveThreshold = 0.4
-negetiveThreshold = 0.45
+rectanglesEps = 0.4
+positiveThreshold = 0.5
+negetiveThreshold = 0.9
 
-cv2.Laplacian(imgToDetect, cv2.CV_16U, imgToDetect, 3)
-# Detect flowers:
-for template in PTemplateList:
-    DetectObjectByTemplate(imgToDetect, template, positiveThreshold, rectangles)
 
-rectangles, weightsNotUsed = cv2.groupRectangles(rectangles, 1, rectanglesEps)
-
-# Detect false positives:
-for template in NTemplateList:
-    index = 0
-    tempRectangles = []
-    #   template = cv2.resize(template, None, None, 0.1, 0.1, cv2.INTER_CUBIC)
-    #   cv2.Laplacian(template, cv2.CV_16U, template, 3)  # sharpen template for more accuracy
+#cv2.Laplacian(imgToDetect, cv2.CV_16U, imgToDetect, 3)
+ODM.DetectPositive(imgToDetect, PTemplateList, positiveThreshold, rectangles)
+if len(rectangles):  # if found matches
     print(len(rectangles))
-    for rec in rectangles:
-        isFalsePositive = DetectFalsePositiveByNegetiveTemplate(imgToDetect, rec, template, negetiveThreshold)
-        if not isFalsePositive:
-            tempRectangles.append(rec)
+    rectangles, weightsNotUsed = cv2.groupRectangles(rectangles, 1, rectanglesEps)
 
-
-    rectangles = tempRectangles
-    print(len(rectangles))
-
-if len(rectangles):
-    DrawRectanglesOnImage(rectangles, imgToDetect)
+ODM.DetectNegative(imgToDetect, NTemplateList, rectangles, negetiveThreshold)
+if len(rectangles):  # if matches still exists
+    ODM.DrawRectanglesOnImage(rectangles, imgToDetect)
 
 cv2.imshow("Final", imgToDetect)
 cv2.waitKey()
